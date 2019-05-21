77396c6bd11ebb30d4f59f5547e2ee9a7f7485a0
JosiahChang <geospikomon@gmail.com>
2019-05-21 13:45:29 -0700

    Implemented basic square and squareGraphics

---
+++ Square.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Square here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Square
+{
+    private SquareGraphics mySquare;
+    
+    /**
+     * Constructor for objects of class Square
+     */
+    public Square()
+    {
+        mySquare = new SquareGraphics(this);
+    }
+    
+    
+    public void clicked()
+    {
+        //call PlayerStatus class method to switch players
+    }
+}
---
+++ SquareGraphics.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class SquareGraphics here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SquareGraphics 
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SquareGraphics
+     */
+    public SquareGraphics(Square square)
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
