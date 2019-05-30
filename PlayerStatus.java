ea98f21a9916f13026299c41d23fe9b0c1c9f7f5
jccccc6 <jeebochung@gmail.com>
2019-05-30 12:53:11 -0700

    Created player status class
    imported packages
    Created constructor
    Brought in x and o pictures

---
+++ PlayerStatus.java
@@ -0,0 +1,31 @@
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.*;
+/**
+ * Write a description of class PlayerStatus here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PlayerStatus extends HBox
+{
+    static final Image noughtImage = new Image(
+       "http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/128/green-cd-icon.png"
+  );
+        static final Image crossImage = new Image(
+      "http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/128/blue-cross-icon.png"
+  );
+        private final ImageView imageView = new ImageView();
+    public PlayerStatus()
+    {
+        
+        
+        
+    }
+    
+    public void currentPlayer()
+    {
+       
+    }
+}
