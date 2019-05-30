06c81a84832d570ff588692e069319a36d545f44
jccccc6 <jeebochung@gmail.com>
2019-05-30 12:38:38 -0700

    Created button class
    Created yes and no buttons, imported necessary materials

---
+++ Buttons.java
@@ -0,0 +1,21 @@
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+/**
+ * Write a description of class Buttons here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Buttons extends HBox
+{
+    public Buttons()
+    {
+        
+    }
+    
+    public void playAgain()
+    {
+        Button playYes = new Button("Play Again");
+        Button playNo = new Button("Leave Game");
+    }
+}
