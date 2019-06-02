15a77e281173347acdb63e79587ccbbee859cc9f
JosiahChang <geospikomon@gmail.com>
2019-06-02 11:45:28 -0700

    added playerStatus and gameGrpahics to class

---
+++ GameGraphics.java
@@ -0,0 +1,19 @@
+
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.*;
+/**
+ * Write a description of class GameGraphics here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GameGraphics extends VBox
+{
+    public GameGraphics(Play game)
+    {
+        getChildren().addAll(game.getMA().getGraphics(), new PlayerStatus());
+        
+    }
+}
