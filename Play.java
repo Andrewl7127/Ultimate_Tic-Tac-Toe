5ed7898fb004752944289db8caed17670104fe0c
jccccc6 <jeebochung@gmail.com>
2019-06-05 13:05:41 -0700

    Did java docs and commenting for play class except for playGame method

--- Play.java
+++ Play.java
@@ -7,18 +7,30 @@ import chn.util.*;
  */
 public class Play
 {
+    //keeps track of player turn
     private String playerTurn;
+    //counts how many moves have been made to determine player turn
     private int count;
+    //keeps track of last turn played
     private int previousX;
     private int previousY;
+    //does the graphics for the player's turn
     private PlayerStatus playStatus;
+    //does the graphics for where the player should go next
     private NextMove move; 
+    //the board used
     private MasterArray game1;
+    //keeps track if the last move reslted in a won big square
     boolean state;
-    private final boolean DEBUG = true;
+    //used for debugging
+    private final boolean DEBUG = false;
     
+    /**
+     * Constructor for objects of class Play
+     */
     public Play()
     {
+      //instantiations
       count = 1;
       playerTurn = "";
       game1 = new MasterArray(this);
@@ -29,21 +41,36 @@ public class Play
       move = new NextMove();
     }
     
+    /**
+     * Gets the board(masterarray)
+     * @return game1
+     */
     public MasterArray getMA()
     {
        return game1;   
     }
     
+    /**
+     * Gets the graphic for who's player turn it is
+     * @return playStatus
+     */
     public PlayerStatus getPS()
     {
         return playStatus;
     }
     
+    /**
+     * Gets the graphic for where the next move should be
+     * @return move
+     */
     public NextMove getNM()
     {
         return move;
     }
     
+    /**
+     * 
+     */
     public void playGame(Square square)
     {
         int[] answer = new int[4];
@@ -124,63 +151,69 @@ public class Play
             } 
             
         }
-    }
-    color(answer);    
-        
-        
      }
-         
-     private void makeTurn(int [] temp, String player, Square current)
-     {
-              game1.doMove(temp[0], temp[1], temp[2], temp[3], player);
-              current.setStatus(playerTurn);
-              state = game1.getMiniArray(temp[0], temp[1]).checkWon();
-              if(player.equals("X"))
-                playStatus.currentPlayerO();
-              if(player.equals("O"))
-                playStatus.currentPlayerX();
-                
+     color(answer);    
+        
+        
      }
      
+     //does the move, does grapics for the clicked square and playerturn
+     private void makeTurn(int [] temp, String player, Square current)
+     {
+         //does the move     
+         game1.doMove(temp[0], temp[1], temp[2], temp[3], player);
+         //changes clicked square to the x or o
+         current.setStatus(playerTurn);
+         //tells you if the square is won after that move
+         state = game1.getMiniArray(temp[0], temp[1]).checkWon();
+         //changes graphics to reflect the current player's turn
+         if(player.equals("X"))
+            playStatus.currentPlayerO();
+         if(player.equals("O"))
+            playStatus.currentPlayerX();
+     }
+     
+     //Passes the correct info to colorMini in MiniArray to color the square
+     //after a square is won
      private void color(int[] answer)
      {
         if(game1.getMiniArray(answer[0], answer[1]).getFinishedSquare().equals("X"))
         {
             game1.getSquare(answer[0], answer[1]).colorMini("X");
             if(DEBUG)
-            System.out.print("X COLOR RUNS");
+                System.out.print("X COLOR RUNS");
         }
         
         if(game1.getMiniArray(answer[0], answer[1]).getFinishedSquare().equals("O"))
         {    
-            if(DEBUG)
-            System.out.print("O COLOR RUNS");
             game1.getSquare(answer[0], answer[1]).colorMini("O");
+            if(DEBUG)
+                System.out.print("O COLOR RUNS");
         }
         
         if(game1.getMiniArray(answer[0], answer[1]).getFinishedSquare().equals("D"))
         {
             game1.getSquare(answer[0], answer[1]).colorMini("D");
             if(DEBUG)
-            System.out.print("D COLOR RUNS");
+                System.out.print("D COLOR RUNS");
         }
      }
      
+     //changes the playerTurn  to reflect the correct player
      private void helpChange(int count)
      {
         if (count % 2 != 0)
-            {
-                playerTurn = "X";
-                if(DEBUG)
-                    System.out.println("Changed to X");
-            }
-            else
-            {
-                playerTurn = "O";
-                if(DEBUG)
-                    System.out.println("Changed to O");
-            } 
-         
+        {
+             playerTurn = "X";
+             if(DEBUG)
+                 System.out.println("Changed to X");
         }
+        else
+        {
+             playerTurn = "O";
+             if(DEBUG)
+                 System.out.println("Changed to O");
+        } 
+     }
   }
     
